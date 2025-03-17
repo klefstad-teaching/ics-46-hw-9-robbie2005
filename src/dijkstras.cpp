d409436fecd88f734abc2f71b01e99668e34161e
robbie2005 <robbiegilmore05@gmail.com>
2025-03-17 05:54:15 +0000

    edited print_path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -39,14 +39,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     return path;
 }
 
-void print_path(const vector<int>& path, int total) {
-    if (path.empty()) {
-        cout << "No path found" << endl;
-        return;
-    }
-    for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i] << " ";
-        if (i < path.size() - 1) cout << endl;
-    }
-    cout << "\nTotal cost is " << total << endl;
+void print_path(const vector<int> & path, int total){
+    for(int i = 0; i < path.size(); ++i){cout << path[i] << " ";}
+    cout << endl << "Total cost is " << total << endl;
 }
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 int main(){
-    Graph test_graph;
-    file_to_graph("medium.txt", test_graph);
-    int src = 3;
+    Graph test;
+    file_to_graph("medium.txt", test);
+    int src = 0;
     vector<int> prev;
-    vector<int> dijkstra_path = dijkstra_shortest_path(test_graph, src, prev);
-    int destination = 1;
+    vector<int> dijkstra_path = dijkstra_shortest_path(test, src, prev);
+    int destination = 4;
     vector<int> short_path = extract_shortest_path(dijkstra_path, prev, destination);
     print_path(short_path, short_path.size());
     return 0;
