8d5c9fd8fb650bd2d0b9522e6597f1d8498cf8d2
robbie2005 <robbiegilmore05@gmail.com>
2025-03-17 06:24:35 +0000

    Edited dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,38 +1,39 @@
 #include "dijkstras.h"
 
 
-vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+vector<int> dijkstra_shortest_path(const Graph& graph, int start, vector<int>& prev_nodes) {
     int num_nodes = graph.size();
-    std::vector<int> shortest_distances(num_nodes, INF);
-    std::vector<bool> visited(num_nodes, false);
+    vector<int> distances(num_nodes, INF);
+    vector<bool> visited(num_nodes, false);
     
     prev_nodes.assign(num_nodes, -1);
     
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     
-    shortest_distances[start_node] = 0;
-    pq.push({0, start_node});
+    distances[start] = 0;  
+    pq.push({0, start});   
 
     while (!pq.empty()) {
-        auto [current_distance, current_node] = pq.top();
+        auto [current_dist, current_node] = pq.top();
         pq.pop();
 
         if (!visited[current_node]) {
-            visited[current_node] = true;
+            visited[current_node] = true;  
 
             for (const Edge& edge : graph[current_node]) {
                 int neighbor = edge.dst;
                 int edge_weight = edge.weight;
 
-                if (!visited[neighbor] && current_distance + edge_weight < shortest_distances[neighbor]) {
-                    shortest_distances[neighbor] = current_distance + edge_weight;
-                    prev_nodes[neighbor] = current_node;
-                    pq.push({shortest_distances[neighbor], neighbor});
+                if (!visited[neighbor] && current_dist + edge_weight < distances[neighbor]) {
+                    distances[neighbor] = current_dist + edge_weight;
+                    prev_nodes[neighbor] = current_node;  
+                    pq.push({distances[neighbor], neighbor}); 
                 }
             }
         }
     }
-    return shortest_distances;
+
+    return distances;  
 }
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
